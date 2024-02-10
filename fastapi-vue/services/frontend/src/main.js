cda6164a12a89e019e35e7f5a435b9bb7584573e
Thomas.N <thomasnolasque@cmail.carleton.ca>
2024-02-10 03:35:50 -0500

    Started Vue Frontend + Docker

--- fastapi-vue/services/frontend/src/main.js
+++ fastapi-vue/services/frontend/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import { createApp } from "vue";
+import axios from 'axios';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:5000/';  // the FastAPI backend
+
+app.use(router);
+app.mount("#app");
