5024f6f378bb09ed6bc3c870cb4980882402f8e7
Thomas.N <thomasnolasque@cmail.carleton.ca>
2024-03-09 03:04:03 -0500

    Planning to potentially deploy the entire app

--- fastapi-vue/services/backend/src/main.py
+++ fastapi-vue/services/backend/src/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:8080"],
+  allow_origins=["http://localhost:8080"], # frontend url
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
